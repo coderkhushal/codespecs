77d96df5ecb0b055c37ae0453217318c4302e69e
coderkhushal <137866759+coderkhushal@users.noreply.github.com>
2023-12-18 19:43:42 +0530

    prod

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
+"use client"
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { useState } from 'react'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,6 +14,8 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  const[cart, setcart] = useState()
+  const[subtotal, setsubtotal] = useState()
   return (
     <html lang="en">
       <body className={`${inter.className} `} >
